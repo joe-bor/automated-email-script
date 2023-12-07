7d8337e2426c2a4941f8150994eba650c1cd4f28
joe-bor <kimborlongan_08@yahoo.com>
2023-12-07 11:49:30 -0800

    re structure message content

--- email_message.py
+++ email_message.py
@@ -17,26 +17,33 @@ load_dotenv(envars)
 sender_email = os.getenv("EMAIL")
 password_email = os.getenv("PASSWORD")
 
+# Take emails from .env
 def get_receiver_emails():
     receiver_emails_str = os.getenv('RECEIVER_EMAILS', "")
     return [email.strip() for email in receiver_emails_str.split(",") if email]
 
-def send_email(subject, receiver_emails, name='friend'):
+def send_email(subject, name='friend'):
     receiver_emails = get_receiver_emails()
     
     # Create the base text message.
     msg = EmailMessage()
     msg["Subject"] = subject
-    msg["From"] = formataddr(("Joe - Software Engineer.", f"{sender_email}"))
+    msg["From"] = formataddr(("Joezari - Software Engineer.", f"{sender_email}"))
     msg["To"] = ", ".join(receiver_emails)
     msg["BCC"] = sender_email
 
     msg.set_content(
         f"""\
         Hi {name},
-        Trying to send and automate email with code.
-        I want to send automated emails to myself (and you too if you want) to constantly remind me of my goals
-        and our 'small wins' to see our progress
+        Trying to send and automated email with code.
+        
+        Goals:
+        - To practice and learn about different Python modules.
+        - Send reminder of long-term goals
+        - Reflect on achievements and appreciate progress
+        
+        Cheers,
+        Joezari
         """
     )
     # Add the html version.  This converts the message into a multipart/alternative
@@ -45,11 +52,28 @@ def send_email(subject, receiver_emails, name='friend'):
     msg.add_alternative(
         f"""\
     <html>
-      <body>
-        <h1>Automated Email created with Python </h1>
+    <body>
+        <h1>Automated Email Created With Python</h1>
         <p>Hi {name},</p>
-        <p>I hope you are well.</p>
-      </body>
+        <p>I wanted to share this automated email created with Python. It's not just an email; it's a way to practice and learn about different Python modules.</p>
+
+        <h2>Goals:</h2>
+        <ul>
+            <li>Send reminders of long-term goals</li>
+            <li>Reflect on achievements and appreciate progress</li>
+        </ul>
+
+        <h2>Icebox:</h2>
+        <ul>
+            <li>Fetch and add inspirational quotes? </li>
+            <li>Fetch and display today's weather? </li>
+            <li> Fetch for a random coding challenge? </li>
+        </ul>
+
+        <p>Cheers,<br>
+        Joezari
+        </p>
+    </body>
     </html>
     """,
         subtype="html",
